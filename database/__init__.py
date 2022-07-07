c26fde9ac488eb99d1e257a2c806283610e1ca21
Anton Zhdan-Pushkin <maqquettex@gmail.com>
2022-07-07 15:06:51 +0300

    Remove old files

--- database/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import jobs
-from . import nfs_requests
-from . import base
-from .redis import redis_conn
--- database/redis.py
+++
@@ -1,5 +0,0 @@
-import redis
-from settings import settings
-
-
-redis_conn = redis.Redis.from_url(settings.redis_url)
