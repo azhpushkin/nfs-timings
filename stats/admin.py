6118d1ba9d3d90dde699f73742775a0935cc583a
Anton Zhdan-Pushkin <maqquettex@gmail.com>
2022-07-08 15:56:06 +0300

    Few admin fixes to board request

--- stats/admin.py
+++ stats/admin.py
@@ -1,14 +1,46 @@
 from django.contrib import admin
+from django.db.models import Count
 
 from stats.models import BoardRequest, Config, Team, Lap
+
+class LapInline(admin.TabularInline):
+    model = Lap
+    readonly_fields = (
+        'id',
+        'team',
+        'pilot_name',
+        'kart',
+        'lap_time'
+    )
+    fields = readonly_fields
+    extra = 0
+    show_change_link = True
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request, obj):
+        return False
 
 
 @admin.register(BoardRequest)
 class BoardRequestAdmin(admin.ModelAdmin):
-    list_display = ('id', 'url', 'created_at', 'status', 'is_processed', 'race_time')
+    list_display = ('id', 'url', 'created_at', 'race_time', 'laps_count')
+    list_per_page = 30
+    inlines = [
+        LapInline
+    ]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        qs = qs.annotate(laps_count=Count('laps'))
+        return qs
 
     def race_time(self, obj):
         return obj.response_json.get('onTablo', {}).get('totalRaceTime', 'NO_TIME')
+
+    def laps_count(self, obj):
+        return obj.laps_count
 
 
 @admin.register(Team)
