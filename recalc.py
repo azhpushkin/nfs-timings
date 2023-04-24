0f908a6fb94b0939114f102a2083bd3bab010282
Anton Zhdan-Pushkin <anton@azhpushkin.me>
2023-04-24 21:35:42 +0300

    Remove obsolete files that work with XLSX

--- recalc.py
+++
@@ -1,9 +0,0 @@
-from stats.models import *
-from stats.processing import *
-
-br = BoardRequest.objects.all().order_by('created_at')
-
-for b in br.filter(status=200).values('pk').iterator():
-   b = BoardRequest.objects.get(pk=b['pk'])
-   print(b, b.created_at)
-   process_json(b)
