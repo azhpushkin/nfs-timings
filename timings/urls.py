926470b38b8bf5883373a0705fb70f5b9a0cf0b6
Anton Zhdan-Pushkin <anton@azhpushkin.me>
2023-05-10 18:54:52 +0300

    Basic auth setup

--- timings/urls.py
+++ timings/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
-urlpatterns = [path('admin/', admin.site.urls), path('', include('stats.urls'))]
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='login.html')),
+    path('logout/', auth_views.LogoutView.as_view()),
+    path('admin/', admin.site.urls),
+    path('', include('stats.urls')),
+]
 urlpatterns += staticfiles_urlpatterns()
