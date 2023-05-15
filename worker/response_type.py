b32f09bd2257864c51ae88510da6060b120805fe
Anton Zhdan-Pushkin <anton@azhpushkin.me>
2023-05-16 01:34:00 +0300

    Add basic pydantic models for response

---
+++ worker/response_type.py
@@ -0,0 +1,84 @@
+import re
+from datetime import time
+from typing import List, Optional
+
+from pydantic import BaseModel, validator
+
+# All the calculations for the race are within 24 hours, so using datetime.time is fine here
+# Lap times have the format of AA:BB.CC, where AA - minutes, BB - seconds, CC - milliseconds
+
+
+LAP_TIME_REGEX = r'(?P<m>\d+:)?(?P<s>\d+)\.(?P<ms>\d+)'
+
+
+class LapTime(str):
+    minutes: int
+    seconds: int
+    ms: int
+
+    def __init__(self, str_value: str):
+        value = re.match(LAP_TIME_REGEX, str_value)
+        if not value:
+            raise ValueError('Bad lap format, expected MM:SS.MS')
+
+        if value.group('m'):
+            self.minutes = int(value.group('m')[:-1])
+        else:
+            self.minutes = 0
+
+        self.seconds = int(value.group('s'))
+
+        # Pad and trip milliseconds so there is only 3 digits left
+        # Rounding is not very important because milliseconds are more than enough
+        self.ms = int(value.group('ms').ljust(3, '0')[:3])
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls
+
+    def __repr__(self):
+        if self.minutes:
+            return f'{self.minutes}:{self.seconds}.{self.ms}'
+        else:
+            return f'{self.seconds}.{self.ms}'
+
+
+class TeamDict(BaseModel):
+    pilotName: str  # name of the current pilot, might be changed during the stint
+    teamName: str  # name of the team, might be changed (needs to be checked)
+
+    # TODO: check rain format
+    lastLap: LapTime  # last lap in seconds in format AA.BB (
+    lastLapS1: LapTime
+    lastLapS2: LapTime
+    lapCount: int
+
+    number: int  # TODO: what does it mean
+    kart: int  # TODO: check if empty
+    totalOnTrack: time
+    bestLapOnSegment: Optional[LapTime]
+
+    # segments: bool  # TODO: define
+
+    @validator('bestLapOnSegment', pre=True)
+    def skip_empty_time_on_first_lap(cls, v: str):
+        return None if v == '' else v
+
+    @validator('totalOnTrack', pre=True)
+    def skip_pit_time(cls, v: str):
+        if '.' in v:
+            # If dot - then time of pit is shown (e.g. 1:30.446)
+            # So we convert it to the 00:1:30 to use as total segment time value
+            return '00:' + v.split('.')[0]
+        else:
+            return v
+
+
+class OnTabloDict(BaseModel):
+    isRace: bool
+    totalRaceTime: time
+    teams: List[TeamDict]
+
+
+class NFSResponseDict(BaseModel):
+    onTablo: OnTabloDict
